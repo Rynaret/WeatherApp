55d3008595d4d60ef77fc6db83525eec00d55d64
Renat <sun.re@bk.ru>
2019-08-18 13:17:30 +0300

    add Grpc.ForecastService

---
+++ Grpc.ForecastService/Services/ForecastService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Interfaces;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GrpcForecastService
+{
+    public class ForecastService : Forecaster.ForecasterBase
+    {
+        #region ctor
+
+        private readonly IServiceProvider _provider;
+
+        public ForecastService(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        #endregion
+
+        public override async Task<AvailableGeoObjectsResponse> GetAvailableGeoObjectsAsync(Empty request, ServerCallContext context)
+        {
+            var scoped = _provider.CreateScope();
+            var geoObjects = await scoped.ServiceProvider.GetService<IRepository>()
+                .Read<GeoObject>()
+                .Select(x => new GeoObjectDto
+                {
+                    Name = x.Name
+                })
+                .ToListAsync();
+            var response = new AvailableGeoObjectsResponse();
+            response.GeoObjects.AddRange(geoObjects);
+            return response;
+        }
+
+
+        public override async Task<ForecastDto> GetForecastAsync(GetForecastRequest request, ServerCallContext context)
+        {
+            var scoped = _provider.CreateScope();
+            var requestDate = request.Date.ToDateTime().Date;
+            var forecast = await scoped.ServiceProvider.GetService<IRepository>()
+                .Query<Forecast>()
+                .Where(x => x.GeoObject.Name == request.GeoObjectName)
+                .Where(x => x.Date == requestDate)
+                .FirstOrDefaultAsync();
+
+            return new ForecastDto
+            {
+                Date = Timestamp.FromDateTime(forecast.Date),
+                MaxTemperature = forecast.MaxTemperature,
+                MinTemperature = forecast.MinTemperature,
+                Precipitation = forecast.Precipitation
+            };
+        }
+    }
+}
---
+++ Grpc.ForecastService/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Domain.Interfaces;
+using Infrastructure.DataAccess.EF;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Grpc.ForecastService
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+
+            services.AddTransient<IRepository, EfRepository>();
+            services.AddDbContextPool<WeatherDbContext>((IServiceProvider serviceProvider, DbContextOptionsBuilder options) =>
+            {
+                options.UseMySql(Configuration.GetConnectionString("weather"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<GrpcForecastService.ForecastService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });
+        }
+    }
+}
